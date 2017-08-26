a2bd019c0cbf117596f28f12c46ef35d00292c82
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-08-26 22:26:02 +0530

    Change files

--- kernel.cpp
+++ kernel.cpp
@@ -1,29 +1,62 @@
+
 #include "types.h"
 #include "gdt.h"
+
 void printf(char* str)
 {
-    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
-    
-    for(int i=0; str[i]!='\0';++i){
-        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x=0,y=0;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+    {
+        switch(str[i])
+        {
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
     }
-    
 }
+
+
 
 typedef void (*constructor)();
-extern "C" constructor* start_ctors;
-extern "C" constructor* end_ctors;
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
 extern "C" void callConstructors()
 {
-    for(constructor* i=start_ctors;i!=end_ctors;i++){
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
         (*i)();
-    }
 }
 
 
-extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
-{ 
-    printf("Hello World");
-    GlobalDescriptortable gdt;
+
+extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
+{
+    printf("Hello World! --- http://www.AlgorithMan.de");
+
+    GlobalDescriptorTable gdt;
+
     while(1);
 }
--- loader.s
+++ loader.s
@@ -6,13 +6,13 @@
     .long MAGIC
     .long FLAGS
     .long CHECKSUM
-    
 
 
 .section .text
 .extern kernelMain
 .extern callConstructors
 .global loader
+
 
 loader:
     mov $kernel_stack, %esp
@@ -21,12 +21,14 @@ loader:
     push %ebx
     call kernelMain
 
+
 _stop:
     cli
     hlt
     jmp _stop
 
-.section .bss 
-.space 2*1024*1024;
+
+.section .bss
+.space 2*1024*1024; # 2 MiB
 kernel_stack:
-    
+
